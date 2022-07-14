294d8224cc72e2e12175f4b3e2172ef9ca4d0877
fan1ech00 <fan1ech000@tuta.io>
2022-07-14 11:42:26 +0300

    remove confirm password

--- Planner.Web.Frontend/src/app/components/signup/signup.component.ts
+++ Planner.Web.Frontend/src/app/components/signup/signup.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AbstractControl, FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import { AuthService } from '../../services/auth.service';
@@ -23,6 +23,7 @@ export class SignupComponent implements OnInit {
     private as: AuthService) { }
 
   async onSubmit() {
+    // this.registerInvalid = false;
     this.formSubmitAttempt = false;
 
     if (this.form.valid) {
@@ -36,36 +37,25 @@ export class SignupComponent implements OnInit {
 
       this.as.signup(username, password)
         .subscribe(res => {
-          this.registerInvalid = false;
+            this.registerInvalid = false;
           },
           error => {
-          if (error.status == 409)
-            this.errors = [{description: "Emailaddress already in use"}];
-          else
-            this.errors = error.error;
-        });
+            if (error.status == 409)
+              this.errors = [{description: "Emailaddress already in use"}];
+            else
+              this.errors = error.error;
+          });
     } else {
       this.formSubmitAttempt = true;
     }
   }
 
-  passwordConfirming(c: AbstractControl): { invalid: boolean } {
-    if (c.get('password')?.value !== c.get('confirm_password')?.value) {
-      return {invalid: true};
-    }
-    return {invalid: false}
-  }
-
   async ngOnInit() {
     this.form = this.fb.group({
       username: ['', [Validators.required, Validators.email]],
-      passwords: this.fb.group({
-        password: ['', [Validators.required]],
-        confirm_password: ['', [Validators.required]],
-      }, {validator: this.passwordConfirming}),
+      password: ['', [Validators.required]],
+      // confirmPassword: ['', [Validators.required]],
     });
-
-    console.log(this.form.get('passwords') as FormGroup);
 
     if (this.as.isAuthenticated) {
       this.router.navigate(['']);
